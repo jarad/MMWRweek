f417bfc7540dd9183b2a8e30f7f2fab723a3c09c
Jarad Niemi <niemi@iastate.edu>
2015-09-29 09:27:06 -0500

    ISOweekday implemented (but only takes dates)

--- ISOweekday.R
+++ ISOweekday.R
@@ -1,20 +1,18 @@
-#' Day of week according to ISO 8601
+#' Day of week according to MMWR
 #'
-#' This function returns the weekday of a given date according to ISO 8601.
-#' It is an substitute for the "\code{\%u}" format which is not implemented on Windows.
+#' This function returns the weekday of a given date according to MMWR.
 #' 
 #' @param date Vector which can be coerced to class \code{Date}
-#' @return An integer vector of weekdays (1-7, Monday is 1)
-#' @author Uwe Block \email{u.block.mz@@googlemail.com}
+#' @return An integer vector of weekdays (1-7, Sunday is 1)
+#' @author Jarad Niemi \email{niemi@@iastate.edu}
 #' @seealso \code{\link{strptime}}
 #' @export
 #' @examples
-#' x <- paste(1999:2011, "-12-31", sep = "")
-#' y <- as.Date(x)
-#' data.frame(date = format(y), weekday = ISOweekday(y))
-#' data.frame(date = x, weekday = ISOweekday(x))
-ISOweekday <- function(date) {
-  date <- as.Date(date)
-  return(as.integer((as.integer(format(date, "%w"))+6) %% 7 + 1))
+#' y <- as.Date(paste(1999:2011, "-12-31", sep = ""))
+#' data.frame(date = format(dates), weekday = weekdays(y), MMWRweekday = MMWRweekday(y))
+MMWRweekday = function(date) {
+  date = as.Date(date)
+  date = factor(weekdays(date), levels=c('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'))
+  return(as.integer(date))
 }
 
