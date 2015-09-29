993c3aa0de9d0bc6a089c9b2d9ffc52a5e7f4e15
Jarad Niemi <niemi@iastate.edu>
2015-09-29 16:51:15 -0500

    junk

--- MMWRweek-internal.R
+++ MMWRweek-internal.R
@@ -1,14 +1,14 @@
 #' Find the day of the week for January 1 of the year of date
 #'
-#' This internal function returns the weekday of January 1 of the year of the date.
+#' This internal function returns the January 1 for the year of the date
 #'
 #' @param date Vector which can be coerced to class \code{Date}
-#' @return An integer vector of weekdays (1-7, Sunday is 1)
-#' @seealso \code{\link{ISOweekday}}
+#' @return A date vector containing January for the year of the date
+#' @seealso \code{\link{MMWRweek}}
 #' @keywords internal
-jan1_weekday = function(date) {
+jan1 = function(date) {
   date = as.Date(date)
   year = format(date, '%Y')
-  jan1 = as.Date(paste(year,'-01-01', sep=''))
-  return(MMWRweekday(jan1))
+  return(as.Date(paste(year,'-01-01', sep='')))
 }
+
