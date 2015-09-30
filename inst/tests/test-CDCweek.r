ad8119e385b9e2f47f75dfb5d3b64e8287ec59ae
Jarad Niemi <niemi@iastate.edu>
2015-09-30 09:48:14 -0500

    tests for CDCweek

---
+++ inst/tests/test-CDCweek.r
@@ -0,0 +1,17 @@
+context('CDCweek')
+
+dates = seq(as.Date('1900-01-01'), as.Date('2015-12-31'), by='day')
+truth = readRDS('CDCweek-truth.rds')
+
+test_that('CDCweek returns a factor vector', {
+  week = CDCweek(dates)
+  expect_that(week, is_a('data.frame'))
+  expect_that(names(week), equals(c('day','week','year')))
+})
+
+test_that('CDCweek returns correct values', {
+  dates_chr = as.Date(dates)
+  expect_that(CDCweek(dates    ), equals(truth))
+  expect_that(CDCweek(dates_chr), equals(truth))
+})
+ 
